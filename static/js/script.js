cccab1fc8b1950c7947f7716a201d8f4312169af
Katelyn Lindsey <katelyn.lindsey77@gmail.com>
2021-11-27 22:29:32 -0800

    Adds the ability to toggle between rgb and hex for palettes.
    Adds JavaScript to handle toggling between rgb and hex text for palettes.

---
+++ static/js/script.js
@@ -0,0 +1,46 @@
+/*
+Katelyn Lindsey
+Color Palette App
+script.js
+*/
+
+/* toggling rgb/hex on palette.html page */
+$(function() {
+	$("#toggle-rgb-hex").click(function() {
+
+		/* first, get all of the colors of the current palette and what
+		type of color value is currently displayed (rgb or hex)*/
+		var colors = $("#toggle-rgb-hex").data("colors");
+		var currentType = $("#color_1").data("colortype");
+		currentType = currentType.toString();
+
+
+		/* convert the string dictionary to a JSON object - requires some initial reformatting*/
+		colors = colors.replace(/'/g, "\"");
+		colors = colors.replace(/\(/g, "\"(");
+		colors = colors.replace(/\)/g, ")\"");
+
+		colors = JSON.parse(colors);
+
+
+		/* if the type is currently rgb, switch to hex */
+		if(currentType == "rgb") {
+
+			for(var key in colors) {
+				$("#" + key).data("colortype", "hex");
+				newText = colors[key][1];
+				$("#" + key).text("#" + newText);
+			}
+			console.log("changed to hex");
+		} else { /* otherwise, switch to rgb */
+
+			for(var key in colors) {
+				$("#" + key).data("colortype", "rgb");
+				newText = colors[key][0];
+				$("#" + key).text("RGB" + newText);
+			}
+			console.log("changed to rgb");
+		}
+
+	});
+});
